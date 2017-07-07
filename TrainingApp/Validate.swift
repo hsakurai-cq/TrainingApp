0d3d5545e28ae2fd988d7a3ed1b6d55dd2adcc1f
hsakurai-cq <h.sakurai@caraquri.com>
2017-07-07 15:24:33 +0900

    SignUpのValidationを設定

--- TrainingApp/Validate.swift
+++ TrainingApp/Validate.swift
@@ -14,4 +14,25 @@ class Validate {
         
         return (true, "")
     }
+    
+    static func signUp(email: String, password: String, passwordCheck: String) -> (result: Bool, error: String) {
+        
+        guard !email.isEmpty else {
+            return (false, R.string.localizable.errorEmpty(R.string.localizable.email()))
+        }
+        
+        guard !password.isEmpty else {
+            return (false, R.string.localizable.errorEmpty(R.string.localizable.password()))
+        }
+        
+        guard !passwordCheck.isEmpty else {
+            return (false, R.string.localizable.errorEmpty(R.string.localizable.passwordCheck()))
+        }
+        
+        guard password == passwordCheck else {
+            return (false, R.string.localizable.errorEmpty(R.string.localizable.passwordMismatch()))
+        }
+        
+        return (true, "")
+    }
 }
